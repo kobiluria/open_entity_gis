413b733c870d8549b291715770098376e36ae58f
Kobi Luria <kobi@luria.net>
2013-08-02 01:43:12 +0300

    geojson importer fix

--- geo_json_importer.py
+++ geo_json_importer.py
@@ -1,1 +1,30 @@
-__author__ = 'open_budget'
+__author__ = 'kobi_luria'
+
+import requests
+import json
+import simplejson
+from simplejson import decoder
+
+def get_geojson_for_muni(search_string):
+
+    f = open('/test.geojson','w+')
+
+
+    url = 'http://nominatim.openstreetmap.org/search?accept-language=en-us&q=' + search_string +\
+          '&format=json&polygon_geojson=1&addressdetails=1'
+
+    result  = requests.get(url)
+
+    data = json.loads(result.text)
+
+    data = data[0]
+
+    if data.has_key('geojson'):
+        polygon =  data.get('geojson')
+
+        json.dump(polygon,f)
+
+
+    f.close()
+
+get_geojson_for_muni('israel')
